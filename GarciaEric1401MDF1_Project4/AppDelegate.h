31190bb803ba1d6af336f33cb0d1eb70e12f19e1
eng618 <eng618@fullsail.edu>
2014-01-28 23:01:50 -0500

    Initial Commit

---
+++ GarciaEric1401MDF1_Project4/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GarciaEric1401MDF1_Project4
+//
+//  Created by Eric Garcia on 1/28/14.
+//  Copyright (c) 2014 Full Sail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GarciaEric1401MDF1_Project4/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GarciaEric1401MDF1_Project4
+//
+//  Created by Eric Garcia on 1/28/14.
+//  Copyright (c) 2014 Full Sail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
